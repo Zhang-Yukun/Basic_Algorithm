22fbd712f0a3fbc83955f01b7afa32f264f4a14d
Zhang-Yukun <2936501245@qq.com>
2023-12-14 20:40:45 +0800

    Finish 787 in cpp; finish 788 in c

--- 787/merge_sort.cpp
+++ 787/merge_sort.cpp
@@ -1,1 +1,31 @@
 #include <iostream>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+int m[N], t[N], n;
+
+void merge_sort(int l, int r);
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &m[i]);
+    merge_sort(0, n - 1);
+    for (int i = 0; i < n; i++) printf("%d ", m[i]);
+    return 0;
+}
+
+void merge_sort(int l, int r) {
+    if (l >= r) return;
+    int middle = l + ((r - l) >> 1), i = l, j = middle + 1, k = 0;
+    merge_sort(l, middle);
+    merge_sort(middle + 1, r);
+    while ((i <= middle) && (j <= r)) {
+        if (m[i] <= m[j]) t[k++] = m[i++];
+        else t[k++] = m[j++];
+    }
+    while (i <= middle) t[k++] = m[i++];
+    while (j <= r) t[k++] = m[j++];
+    k = 0;
+    for (i = l; i <= r; i++) m[i] = t[k++];
+}
--- 788/inverse_pair.c
+++ 788/inverse_pair.c
@@ -2,40 +2,33 @@
 
 #define N 100005
 
-int m[N], t[N], n;
-long long p;
+int a[N], t[N], n;
+long long res;
 
-void merge_sort(int m[], int left, int right);
+void merge_sort(int l, int r);
 
 int main() {
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &m[i]);
-    }
-    merge_sort(m, 0, n - 1);
-    printf("%lld", p);
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    merge_sort(0, n - 1);
+    printf("%lld", res);
     return 0;
 }
 
-void merge_sort(int m[], int left, int right) {
-    if (left >= right) {
-        return;
-    }
-    int middle = left + ((right - left) >> 1);
-    merge_sort(m, left, middle);
-    merge_sort(m, middle + 1, right);
-    int i = left, j = middle + 1, k = 0;
-    while ((i <= middle) && (j <= right)) {
-        if (m[i] <= m[j]) {
-            t[k++] = m[i++];
-        } else {
-            p += (middle - i + 1);
-            t[k++] = m[j++];
+void merge_sort(int l, int r) {
+    if (l >= r) return;
+    int m = l + ((r - l) >> 1), i = l, j = m + 1, k = 0;
+    merge_sort(l, m);
+    merge_sort(m + 1, r);
+    while ((i <= m) && (j <= r)) {
+        if (a[i] <= a[j]) t[k++] = a[i++];
+        else {
+            res += (m  - i + 1);
+            t[k++] = a[j++];
         }
     }
-    while (i <= middle) t[k++] = m[i++];
-    while (j <= right) t[k++] = m[j++];
-    for (int i = 0; i < k; i++) {
-        m[left + i] = t[i];
-    }
+    while (i <= m) t[k++] = a[i++];
+    while (j <= r) t[k++] = a[j++];
+    k = 0;
+    for (i = l; i <= r; i++) a[i] = t[k++];
 }
